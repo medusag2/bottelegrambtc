e8917a1f58fdaa9d6642f1b2f2f40ee235618f09
medusag2 <farruxashuraliyev62@gmail.com>
2025-07-25 14:26:55 +0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import aiohttp
-
 async def get_exchange_rate(from_currency, to_currency):
     url = f"https://api.exchangerate.host/convert?from={from_currency}&to={to_currency}"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             data = await resp.json()
-            return data["result"]
+            rate = data.get("result")
+            if rate is None:
+                raise ValueError("Exchange rate not found.")
+            return rate
