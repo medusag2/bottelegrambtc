89ccaf7a4cd4a46a9a24d7817b3006545b8ec0ee
medusag2 <farruxashuraliyev62@gmail.com>
2025-07-26 23:07:11 +0500

    Create users.py

---
+++ users.py
@@ -0,0 +1,7 @@
+user_languages = {}
+
+def set_user_language(user_id: int, language: str):
+    user_languages[user_id] = language
+
+def get_user_language(user_id: int) -> str:
+    return user_languages.get(user_id, "en")
